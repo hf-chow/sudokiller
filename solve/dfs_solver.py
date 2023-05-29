2fdaa2802755b9e1d1e4b1d013b0e3bedee02bb9
Hon Chow <honfungchow@gmail.com>
2023-05-30 01:04:21 +0800

    updated dfs_solver.py

--- solve/dfs_solver.py
+++ solve/dfs_solver.py
@@ -14,12 +14,24 @@ Borrowing idea from Peter Norvig's incredible blog post:
     constraint propagation.
 """
 
-rows = "ABCDEFGHI"
-cols = "123456789"
-
 def permutate(X, Y):
     return [x+y for x in X for y in Y]
 
 def gen_cells(rows, cols):
     return permutate(rows, cols)
+
+
+rows = "ABCDEFGHI"
+cols = "123456789"
+
+cells = gen_cells(rows, cols)
+units = [permutate(r, c) for r in ["ABC", "DEF", "GHI"] for c in ["123", "456", "789"]] 
+unit_list = []
+
+for cell in cells:
+    for unit in units:
+        if cell in unit:
+            unit_list.append((cell, unit))
+
+unit_dict = dict(unit_list)
 
