08d53b46e4730a5aad7df193944d8c5955c16391
Hon Chow <honfungchow@gmail.com>
2023-06-01 01:26:58 +0800

    implemented assign_n_check and elim_n_check

--- solve/dfs_solver.py
+++ solve/dfs_solver.py
@@ -33,11 +33,10 @@ unit_list = ([permutate(r, c) for r in ["ABC", "DEF", "GHI"] for c in ["123", "4
 units = dict([(cell, [unit for unit in unit_list if cell in unit]) for cell in cells])
 peers = dict([(cell, set(flatten(units[cell])) - set([cell])) for cell in cells])
 
-
-
 def assign_n_check(group, cell, to_delete):
     """
-    Assign the value to a cell and check all the other member of its unit or 
+    Assign the value to a cell and the delete the value from the other members'
+    possible values list. Check all the other member of its unit or 
     its peer for the remaining possible values, whether the value is the only
     possible value in any of the cell of the group,. group can be either 
     unit or peer.
@@ -49,13 +48,21 @@ def assign_n_check(group, cell, to_delete):
         for to_check in possible_val:
             elim_n_check(group, cell, to_check) 
 
-    
-
-    pass
-
 def elim_n_check(group, cell, to_delete):
     """
-    Check if there are any cell with len(group[cell]) == 1. If true, call
-    assign_n_check on it. 
+    Delete the value that is taken by another cell of the group. Check if the
+    remaining possible values reduce to zero. If so, call assign_n_check to 
+    assign and to recursively check the other members
     """
-    
+
+    possible_vals_remain = group[cell].replace(to_delete, "")
+
+    if len(group[cell]) == 1:
+        assign_n_check(group, cell, group[cell][0])
+
+    for peer in peers[cell]:
+        elim_n_check(group, peer, group[cell][0])
+
+    for unit in units(cell):
+        elim_n_check(group, unit, group[cell][0])
+
